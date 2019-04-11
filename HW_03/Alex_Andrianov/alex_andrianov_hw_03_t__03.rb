c5dd0f6b862aea656e2f597babaabad94f44c20c
alex1Der <43265961+alex1Der@users.noreply.github.com>
2019-04-11 16:28:16 +0300

    Updated task_3

--- HW_03/Alex_Andrianov/alex_andrianov_hw_03_t__03.rb
+++ HW_03/Alex_Andrianov/alex_andrianov_hw_03_t__03.rb
@@ -6,7 +6,7 @@ def task_3(string)
   string.each_line do |line|
     if line.include?('Calling core') && first_string == ''
       first_string = line.match(/(\d{2}\:\d{2}:\d{2}.\d{1})/)
-    else
+    elsif line.include?('Calling core') && second_string == ''
       second_string = line.match(/(\d{2}\:\d{2}:\d{2}.\d{1})/)
     end
   end
@@ -14,9 +14,13 @@ def task_3(string)
 end
 
 def helper(first, second)
-  first_point = DateTime.strptime(first.to_s,
-                                  '%H:%M:%S.%L').strftime('%Q').to_i
-  second_point = DateTime.strptime(second.to_s,
-                                   '%H:%M:%S.%L').strftime('%Q').to_i
-  first_point - second_point / -1000.0
+  if second != ''
+    first_point = DateTime.strptime(first.to_s,
+                                    '%H:%M:%S.%L').strftime('%Q').to_i
+    second_point = DateTime.strptime(second.to_s,
+                                     '%H:%M:%S.%L').strftime('%Q').to_i
+    first_point - second_point / -1000.0
+  else
+    return 0
+  end
 end
