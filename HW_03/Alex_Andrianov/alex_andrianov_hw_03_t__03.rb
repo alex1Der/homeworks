83bca78d49c5142fd98ec216734aac783aa5170f
alex1Der <43265961+alex1Der@users.noreply.github.com>
2019-04-11 15:52:17 +0300

    Updated task_3

--- HW_03/Alex_Andrianov/alex_andrianov_hw_03_t__03.rb
+++ HW_03/Alex_Andrianov/alex_andrianov_hw_03_t__03.rb
@@ -2,28 +2,21 @@ require 'date'
 require 'time'
 
 def task_3(string)
-  second_string = '', first_string = ''
-  new_helper(first_string, second_string, string)
+  first_string = '', second_string = ''
+  string.each_line do |line|
+    if line.include?('Calling core') && first_string == ''
+      first_string = line.match(/(\d{2}\:\d{2}:\d{2}.\d{1})/)
+    else
+      second_string = line.match(/(\d{2}\:\d{2}:\d{2}.\d{1})/)
+    end
+  end
   helper(first_string, second_string)
 end
 
-def helper(first_string, second_string)
-  first_point = DateTime.strptime(first_string.to_s,
+def helper(first, second)
+  first_point = DateTime.strptime(first.to_s,
                                   '%H:%M:%S.%L').strftime('%Q').to_i
-  second_point = DateTime.strptime(second_string.to_s,
+  second_point = DateTime.strptime(second.to_s,
                                    '%H:%M:%S.%L').strftime('%Q').to_i
-  a = (first_point - second_point) / 1000.0
-  a.to_s
-end
-
-def new_helper(string1, _string2, main_string)
-  main_string.each_line do |line|
-    if line.include?('Calling core')
-      if string1 == ''
-        string1 = line.match(/(\d{2}\:\d{2}:\d{2}.\d{1})/)
-      else
-        _string2 = line.match(/(\d{2}\:\d{2}:\d{2}.\d{1})/)
-      end
-    end
-  end
+  first_point - second_point / -1000.0
 end
