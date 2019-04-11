5f01b26115ddfcd421b9f7fdd8fb2954fe59294e
alex1Der <43265961+alex1Der@users.noreply.github.com>
2019-04-11 15:09:05 +0300

    Added 2 spaces near "FROM:"

--- HW_03/Alex_Andrianov/alex_andrianov_hw_03_t__02.rb
+++ HW_03/Alex_Andrianov/alex_andrianov_hw_03_t__02.rb
@@ -5,7 +5,7 @@ def task_2(string)
     date = line[/[\[].*[\]]/]
     rec = line[/T .* H/]
     if send && date && rec
-      array << "#{date[1..-2]}FROM:#{send[0..-4]}TO:#{rec[1..-3].upcase}"
+      array << "#{date[1..-2]} FROM: #{send[0..-4]}TO:#{rec[1..-3].upcase}"
     end
   end
   array
