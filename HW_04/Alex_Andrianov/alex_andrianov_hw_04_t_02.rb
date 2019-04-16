ccd5e9b53d2a93745a516770c38b45c7bdf2e2fb
alex1Der <43265961+alex1Der@users.noreply.github.com>
2019-04-16 19:48:14 +0300

    Style changes

--- HW_04/Alex_Andrianov/alex_andrianov_hw_04_t_02.rb
+++ HW_04/Alex_Andrianov/alex_andrianov_hw_04_t_02.rb
@@ -5,7 +5,7 @@ def task_4_2(input)
     input
   else
     new_inp = YAML.safe_load(input)
-    new_inp.map { |key, value| [key.to_sym => value_sel(value)] }.flatten
+    new_inp.map { |key, value| [key.to_sym => value_selection(value)] }.flatten
   end
 end
 
@@ -18,17 +18,11 @@ def default_magic_number(default_value)
   default_value['pool'].to_i * default_value['timeout'].to_i
 end
 
-def value_sel(selected_value)
+def value_selection(selected_value)
   result_value = {}
-  if selected_value.key?('database')
-    result_value[:db] = selected_value['database']
-  end
-  if selected_value.key?('username')
-    result_value[:user] = selected_value['username']
-  end
-  if selected_value.key?('password')
-    result_value[:password] = selected_value['password']
-  end
+  result_value[:db] = selected_value['database'] if selected_value.key?('database')
+  result_value[:user] = selected_value['username'] if selected_value.key?('username')
+  result_value[:password] = selected_value['password'] if selected_value.key?('password')
   result_value[:magic_number] = default_magic_number(selected_value)
   result_value
 end
