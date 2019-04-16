bdb147f96ae054fb3caa8b71bd6928b209d5719d
alex1Der <43265961+alex1Der@users.noreply.github.com>
2019-04-16 23:29:13 +0300

    Fixed error with camelcase

--- HW_04/Alex_Andrianov/alex_andrianov_hw_04_t_04.rb
+++ HW_04/Alex_Andrianov/alex_andrianov_hw_04_t_04.rb
@@ -1,4 +1,4 @@
 def task_4_4(string)
-  true if string == string.reverse && string.empty? != true
+  true if string.upcase == string.upcase.reverse && string.empty? != true
   false if string.empty? == true
 end
