662ddf5d76a723a893a4a2b4bf9f2480e680a17e
alex1Der <43265961+alex1Der@users.noreply.github.com>
2019-04-16 19:01:55 +0300

    Style fixes for rubocop

--- HW_04/Alex_Andrianov/alex_andrianov_hw_04_t_01.rb
+++ HW_04/Alex_Andrianov/alex_andrianov_hw_04_t_01.rb
@@ -1,18 +1,15 @@
-def task_4_1(n)
-  if n > 0 && n.is_a?
-    fibonacci_rec(n)
-  else
-    return 0
-  end
+def task_4_1(num)
+  fibonacci_rec(num) if num > 0 && num.is_a? 
+  0 if num == 0
 end
 
-def fibonacci_rec(n)
-  fibonacci(0, 1,1, n)
+def fibonacci_rec(num)
+  fibonacci(0, 1, 1, num)
 end
 
-def fibonacci(first, second, count, n)
-  if count <= n
-    put first.to_s + ' '
-    fibonacci(second, first + second, count + 1, n)
+def fibonacci(first, second, count, num)
+  if count <= num
+    first.to_s + ' '
+    fibonacci(second, first + second, count + 1, num)
   end
 end
