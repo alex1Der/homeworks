e055b458d0f5c27ea35ac32abc01391603cadd9e
alex1Der <43265961+alex1Der@users.noreply.github.com>
2019-04-16 23:45:26 +0300

    Changed return value

--- HW_04/Alex_Andrianov/alex_andrianov_hw_04_t_04.rb
+++ HW_04/Alex_Andrianov/alex_andrianov_hw_04_t_04.rb
@@ -1,4 +1,3 @@
 def task_4_4(string)
-  true if string.upcase == string.upcase.reverse && string.empty? != true
-  false if string.empty? == true
+  string.upcase == string.upcase.reverse if string.empty? != true
 end
